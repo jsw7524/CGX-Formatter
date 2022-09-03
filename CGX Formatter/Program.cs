49286402ffa22e9925d1f8e563fb27b87de26a87
jsw7524 <jsw7524@gmail.com>
2022-09-03 14:10:48 +0800

    Add project files.

---
+++ CGX Formatter/Program.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CGX_Formatter
+{
+    public class Token
+    {
+        public string TypeName { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class Tokenizer
+    {
+        public IEnumerable<Token> GetTokens(string source)
+        {
+            Regex regex = new Regex(@"(?<assignment>=)|(?<separator>;)|(?<null>null)|(?<boolean>false|true)|(?<lp>\()|(?<rp>\))|(?<number>-?\d+)|(?<str>'[\w \(\)=;]+')");
+            List<Token> tmp = new List<Token>();
+            foreach (Match m in regex.Matches(source))
+            {
+                for (int i = 1; i < m.Groups.Count; i++)
+                {
+                    if (m.Groups[i].Length > 0)
+                    {
+                        tmp.Add(new Token() { TypeName = m.Groups[i].Name, Content = m.Value });
+                        break;
+                    }
+                }
+            }
+            return tmp;
+        }
+    }
+
+    public class TokenIndexer
+    {
+        Stack<int> _stack = new Stack<int>();
+        int _position = 0;
+        public int Position { get { return _position; } set { _position = value; } }
+        List<Token> _tokens;
+        string _currentTokenContent = null;
+        public TokenIndexer(IEnumerable<Token> tokens)
+        {
+            _tokens = tokens.ToList();
+        }
+
+        public void SavePosition()
+        {
+            _stack.Push(_position);
+        }
+
+
+        public void PopPosition()
+        {
+            _stack.Pop();
+        }
+
+
+        public void RestorePosition()
+        {
+            _position = _stack.Pop();
+        }
+
+        public string GetTokenContent
+        {
+            get { return _currentTokenContent; }
+        }
+
+        public bool IsComplete { get { return _position == _tokens.Count(); } }
+
+        public bool Check(string s)
+        {
+            bool result = false;
+            _currentTokenContent = null;
+            if (_tokens[_position].TypeName == s)
+            {
+                _currentTokenContent = _tokens[_position].Content;
+                _position++;
+                result = true;
+            }
+            return result;
+        }
+
+    }
+
+    public class Parser
+    {
+        // the implementation of order of gramar expansion is important
+
+        // Start -> ELEMENT
+        // ELEMENT -> BLOCK | PRIMITIVE_TYPE | KEY_VALUE
+        // BLOCK -> ( SEQUENCE )
+        // SEQUENCE -> ELEMENT | ELEMENT;SEQUENCE | EMPTY
+        // PRIMITIVE_TYPE -> NUMBER | BOOLEAN | STRING | NULL
+        // KEY_VALUE -> STRING = BLOCK | STRING = PRIMITIVE_TYPE
+
+        public bool Parse(string source)
+        {
+            try
+            {
+                Tokenizer tokenizer = new Tokenizer();
+                var tokens = tokenizer.GetTokens(source);
+                TokenIndexer tokenIndexer = new TokenIndexer(tokens);
+                bool result = START(tokenIndexer);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public bool START(TokenIndexer indexer)
+        {
+            indexer.SavePosition();
+            if (ELEMENT(indexer))
+            {
+                indexer.PopPosition();
+
+                if (indexer.IsComplete)
+                {
+                    return true;
+                }
+
+            }
+            indexer.RestorePosition();
+            return false;
+        }
+
+        public bool ELEMENT(TokenIndexer indexer)
+        {
+            indexer.SavePosition();
+            if (BLOCK(indexer))
+            {
+                indexer.PopPosition();
+                return true;
+            }
+            indexer.RestorePosition();
+
+            indexer.SavePosition();
+            if (KEY_VALUE(indexer))
+            {
+                indexer.PopPosition();
+                return true;
+            }
+            indexer.RestorePosition();
+
+            indexer.SavePosition();
+            if (PRIMITIVE_TYPE(indexer))
+            {
+                indexer.PopPosition();
+                return true;
+            }
+            indexer.RestorePosition();
+
+
+            return false;
+        }
+
+        public bool KEY_VALUE(TokenIndexer indexer)
+        {
+            indexer.SavePosition();
+            if (STRING(indexer) && indexer.Check("assignment") && BLOCK(indexer))
+            {
+                indexer.PopPosition();
+                return true;
+            }
+            indexer.RestorePosition();
+
+            indexer.SavePosition();
+            if (STRING(indexer) && indexer.Check("assignment") && PRIMITIVE_TYPE(indexer))
+            {
+                indexer.PopPosition();
+                return true;
+            }
+            indexer.RestorePosition();
+
+            return false;
+        }
+
+
+
+        public bool BLOCK(TokenIndexer indexer)
+        {
+            indexer.SavePosition();
+            if (indexer.Check("lp") && SEQUENCE(indexer) && indexer.Check("rp"))
+            {
+                indexer.PopPosition();
+                return true;
+            }
+            indexer.RestorePosition();
+            return false;
+        }
+
+        public bool SEQUENCE(TokenIndexer indexer)
+        {
+
+            indexer.SavePosition();
+            if (ELEMENT(indexer) && indexer.Check("separator") && SEQUENCE(indexer))
+            {
+                indexer.PopPosition();
+                return true;
+            }
+            indexer.RestorePosition();
+
+            indexer.SavePosition();
+            if (ELEMENT(indexer))
+            {
+                indexer.PopPosition();
+                return true;
+            }
+            indexer.RestorePosition();
+
+
+            return true;
+        }
+
+        public bool PRIMITIVE_TYPE(TokenIndexer indexer)
+        {
+            indexer.SavePosition();
+            if (NUMBER(indexer))
+            {
+                indexer.PopPosition();
+                return true;
+            }
+            indexer.RestorePosition();
+
+            indexer.SavePosition();
+            if (BOOLEAN(indexer))
+            {
+                indexer.PopPosition();
+                return true;
+            }
+            indexer.RestorePosition();
+
+            indexer.SavePosition();
+            if (STRING(indexer))
+            {
+                indexer.PopPosition();
+                return true;
+            }
+            indexer.RestorePosition();
+
+            indexer.SavePosition();
+            if (NULL(indexer))
+            {
+                indexer.PopPosition();
+                return true;
+            }
+            indexer.RestorePosition();
+
+            return false;
+        }
+
+        public bool NUMBER(TokenIndexer indexer)
+        {
+            bool result = indexer.Check("number");
+            if (true == result)
+            {
+                return true;
+            }
+            return result;
+        }
+        public bool BOOLEAN(TokenIndexer indexer)
+        {
+            bool result = indexer.Check("boolean");
+            if (true == result)
+            {
+                return true;
+            }
+            return result;
+        }
+
+        public bool STRING(TokenIndexer indexer)
+        {
+            bool result = indexer.Check("str");
+            if (true == result)
+            {
+                return true;
+            }
+            return result;
+        }
+
+        public bool NULL(TokenIndexer indexer)
+        {
+            bool result = indexer.Check("null");
+            if (true == result)
+            {
+                return true;
+            }
+            return result;
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CGX_Formatter;
+using System.Linq;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Tokenizer tokenizer = new Tokenizer();
+            var tokens = tokenizer.GetTokens("(('k1'=1);('k2'=2))");
+            Assert.AreEqual(13, tokens.Count());
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Tokenizer tokenizer = new Tokenizer();
+            var tokens = tokenizer.GetTokens("(0;1;2)");
+            Assert.AreEqual(7, tokens.Count());
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Tokenizer tokenizer = new Tokenizer();
+            var tokens = tokenizer.GetTokens("true");
+            Assert.AreEqual(1, tokens.Count());
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Tokenizer tokenizer = new Tokenizer();
+            var tokens = tokenizer.GetTokens("( )");
+            Assert.AreEqual(2, tokens.Count());
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Tokenizer tokenizer = new Tokenizer();
+            var tokens = tokenizer.GetTokens("'spaces and    tabs'");
+            Assert.AreEqual(1, tokens.Count());
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Parser parser= new Parser();
+            bool result = parser.Parse("(('k1'=1);('k2'=2))");
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Parser parser = new Parser();
+            bool result = parser.Parse(@"(
+    'menu'=
+    (
+        'id'= 'file';
+        'value'= 'File';
+        'popup'=
+        (
+            'menuitem'=
+            (
+                ( 'value'= 'New'; 'onclick'= 'CreateNewDoc()' );
+                ( 'value'= 'Open'; 'onclick'= 'OpenDoc()' );
+                ( 'value'= 'Close'; 'onclick'= 'CloseDoc()' )
+            )
+        ); ()
+    )
+)
+");
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Parser parser = new Parser();
+            bool result = parser.Parse(@"
+'users'=(('id'=10;
+'name'='Serge';
+'roles'=('visitor';
+'moderator'
+));
+('id'=11;
+'name'='Biales'
+);
+true
+)
+");
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Parser parser = new Parser();
+            bool result = parser.Parse(@"
+( 'user'= (
+    'key'='1= t(c)(';
+    'valid'=false
+  );
+  'user'= (
+    'key'=' = ; ';
+    'valid'= true
+  ); ()
+​)
+");
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            Parser parser = new Parser();
+            bool result = parser.Parse("true");
+            Assert.AreEqual(result, true);
+        }
+
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+            Parser parser = new Parser();
+            bool result = parser.Parse("(1=1)");
+            Assert.AreEqual(result, false);
+        }
+    }
+}
